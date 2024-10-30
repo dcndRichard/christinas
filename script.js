082ba41293a1c29c24cfc330aa6c2f646441164b
Duane Richard <duanerichard76@yahoo.com>
2024-10-30 16:31:58 -0700

    exersice list script

--- script.js
+++ script.js
@@ -7,6 +7,92 @@ function fetch1(callback) {
       callback(new Error(), undefined);
     }
   });
-  request.open("GET",);
+  request.open("GET", "/workout.json");
   request.send();
 }
+/* -------------------- */
+let exerciseListItems = document.querySelector("#exercise-list").children;
+let exerciseDisplay = document.querySelector("#exercise-display");
+
+// for (let listItem of exerciseListItems) {
+//   listItem.addEventListener("click", e => {
+//     // console.log(e.target.id);
+//     toggle();
+//   });
+// }
+
+// function toggle() {
+//   if (exerciseDisplay.children.length === 0) {
+//     fetch1(populateContentCallback);
+//   } else {
+//     exerciseDisplay.innerHTML = "";
+//   }
+// }
+
+for (let listItem of exerciseListItems) {
+  listItem.addEventListener("click", e => {
+    let elements = ``;
+    fetch1((err, data) => {
+      if (err) {
+        console.log(err);
+      } else {
+        for (let item of data) {
+          if (e.target.id === item.bodyPart) {
+            console.log(item.workout);
+            for (let exercise of item.workout) {
+              elements += ` <p>${exercise.exercise}</p>`;
+            }
+              console.log(exerciseDisplay.innerHTML.valueOf());
+            exerciseDisplay.innerHTML = (exerciseDisplay.children.length == 0) ? elements : " " ;
+            elements = "";
+            break;
+          }
+        }
+      }
+    });
+  });
+}
+
+// function populateContentCallback(err, data){
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log(data)
+//         for (let i of data) {
+//             if (i.bodyPart === "chest") {
+//                 console.log(i)
+//             }
+//         }
+//         data[ 0 ].workout.forEach(item => {
+//             exerciseDisplay.innerHTML += `
+//                           <p>${item.exercise}</p>
+//                       `;
+//         });
+//     }
+// }
+
+/* 
+
+let exerciseListItems = document.querySelector("#exercise-list").children;
+for (let listItem of exerciseListItems) {
+  listItem.addEventListener("click", e => {
+    console.log(e.target.id);
+    let exerciseDisplay = document.querySelector("#exercise-display");
+    if (exerciseDisplay.children.length === 0)
+      fetch1((err, data) => {
+        if (err) {
+          console.log(err);
+        } else {
+          data[0].workout.forEach(item => {
+            exerciseDisplay.innerHTML += `
+                          <p>${item.exercise}</p>
+                      `;
+          });
+        }
+      });
+    else {
+      exerciseDisplay.innerHTML = "";
+    }
+  });
+}
+*/
